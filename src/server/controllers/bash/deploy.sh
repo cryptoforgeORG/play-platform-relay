fda52fa042890a730e6c8c4241b7664d2d237f69
Konstantin Yurchenko Jr <konstantin.k.yurchenko@gmail.com>
2021-05-20 06:31:38 -0700

    Add directory create.

--- src/server/controllers/bash/deploy.sh
+++ src/server/controllers/bash/deploy.sh
@@ -23,6 +23,7 @@ rm -rf masters-of-conquest-*
 wget -O masters-of-conquest-headless.zip $URL
 
 echo "unzip"
+mkdir masters-of-conquest-headless
 unzip -d masters-of-conquest-headless masters-of-conquest-headless.zip
 
 # echo "set up logging"
